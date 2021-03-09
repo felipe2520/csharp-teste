dc5356613cbc6a7bdb50294c90127fa61e5f4528
bald <bald@pop-os.localdomain>
2021-03-08 22:05:49 -0300

    First programming c#

---
+++ teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Felipe";
+            Console.WriteLine("Hello World!" +name);
+        }
+    }
+}
